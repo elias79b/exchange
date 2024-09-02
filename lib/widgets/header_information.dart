0251555da680b80d99133289c07996b95a93c07a
elias79b <eliasbabakhani@gmail.com>
2024-09-02 08:01:30 +0330

    done part3

--- lib/widgets/header_information.dart
+++ lib/widgets/header_information.dart
@@ -13,9 +13,34 @@ Widget HeaderInformation(BuildContext context){
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         Image.asset('assets/bitcoin.png',width: ConstSize.logo_size,height: ConstSize.logo_size,),
-        SizedBox(width: 5,),
+        SizedBox(width: 8,),
         Text('BTC/USDT',style: Theme.of(context).textTheme.labelLarge),
-        Icon(Icons.keyboard_arrow_down_outlined,color: CLR.grey_text,size: 18,)
+        Icon(Icons.keyboard_arrow_down_outlined,color: CLR.grey_text,size: 18,),
+        SizedBox(width: 40,),
+        Column(
+          children: [
+            Text('72,120',style: Theme.of(context).textTheme.labelLarge!.copyWith(fontSize: ConstSize.small_text_size),),
+            SizedBox(height: 4,),
+            Text('+5.2',style: Theme.of(context).textTheme.displaySmall!.copyWith(color: CLR.greenbtn_color,fontWeight: FontWeight.w600),),
+          ],
+        ),
+        SizedBox(width: 40,),
+        Column(
+          children: [
+            Text('24h High',style: Theme.of(context).textTheme.displaySmall!.copyWith(fontSize: ConstSize.small_text_size),),
+            SizedBox(height: 4,),
+            Text('73,00',style: Theme.of(context).textTheme.labelLarge!.copyWith(fontSize: ConstSize.small_text_size),),
+          ],
+        ),
+        SizedBox(width: 40,),
+        Column(
+          children: [
+            Text('24h Volume',style: Theme.of(context).textTheme.displaySmall!.copyWith(fontSize: ConstSize.small_text_size),),
+            SizedBox(height: 4,),
+            Text('10,00',style: Theme.of(context).textTheme.labelLarge!.copyWith(fontSize: ConstSize.small_text_size),),
+          ],
+        ),
+
       ],
     ),
   );
